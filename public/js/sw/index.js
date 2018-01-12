1e275d6b784dc55690163389c7be28a59a9a44a7
Whisnant <brandon.whisnant@xpta010166-mbp.hps.com>
2018-01-12 15:27:16 -0500

    added additional code for fetch

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,3 +1,23 @@
 self.addEventListener('fetch', function(event) {
-  console.log('hello world');
+  //this intercepts all fetches and is pretty damn cool!
+
+  if (event.request.url.endsWith('.jpg')) {
+    event.respondWith(
+      //the following is amazing and needs to be investigated more!!
+      // new Response('<strong class="a-winner-is-me" style="color:red; font-size: 1.5em;">Hello</strong>', {
+      //   headers: {'content-type': 'text/html'}
+      //})
+
+      //this serves a gif as a response to everything
+      //added if statement above---this now only responds if the url ends with .jpg ---> this replaces all images
+
+      fetch('/imgs/dr-evil.gif')
+
+
+
+    );
+  }
+  
 });
+
+//fetch lets you make network requests and read the response
